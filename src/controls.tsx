ae94aa67dfa86aa8c2962bb0538f92835f32fb35
DALLAS\Thomson <john_thomson@sil.org>
2016-07-19 12:12:51 -0500

    Remove a line added by accident

--- src/controls.tsx
+++ src/controls.tsx
@@ -1,5 +1,4 @@
 /// <reference path="../typings/index.d.ts" />
-/// <reference path="../node_modules/@types/classnames/index.d.ts" />
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Navigation from "./navigation";
